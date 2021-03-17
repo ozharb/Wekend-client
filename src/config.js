3d64bcf9e761d6361c2ce0a60989618d6d9ef19d
Ozzie Harb <ozharb@gmail.com>
2021-03-17 10:33:29 -0700

    config file clean up

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE,
-    //TestingEndpoint://
-    API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'WEkend-client-auth-token',
-    
+  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  PORT: process.env.PORT || 8080,
+  TOKEN_KEY: 'WEkend-client-auth-token',
   }
