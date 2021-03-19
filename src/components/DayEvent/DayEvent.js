1fc0544e4b23b89d894f41298fdcdfb98409591d
Ozzie Harb <ozharb@gmail.com>
2021-03-19 08:19:33 -0700

    day events color pattern

--- src/components/DayEvent/DayEvent.js
+++ src/components/DayEvent/DayEvent.js
@@ -1,5 +1,5 @@
 import toast, { Toaster } from 'react-hot-toast';
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import WekendApiService from '../../services/Wekend-api-service'
 import MediaQuery from 'react-responsive'
@@ -20,116 +20,115 @@ export default class DayEvent extends Component {
             expand: !this.state.expand
         })
     }
-    render() {  
-               const { event } = this.props
+    render() {
+        const { event } = this.props
         let username = 'user'
-        const user =  window.localStorage.getItem(username)
+        const user = window.localStorage.getItem(username)
         const attending = (event.attendees.length)
-        ? (!!event.attendees.find(i=>i.username===user))
-        : false
-        
-        const hostName = event.Event_Host.length>10
-        ? <>{[...event.Event_Host].splice(0,10).join('')}-
-             <br/>
-             {[...event.Event_Host].splice(10).join('')}</>
-        : event.Event_Host
+            ? (!!event.attendees.find(i => i.username === user))
+            : false
 
-        const rsvpLink = (attending)?`cancel-rsvp`:`rsvp`
+        const hostName = event.Event_Host.length > 10
+            ? <>{[...event.Event_Host].splice(0, 10).join('')}-
+                <br />
+                {[...event.Event_Host].splice(10).join('')}</>
+            : event.Event_Host
+
+        const rsvpLink = (attending) ? `cancel-rsvp` : `rsvp`
         const path = `/${rsvpLink}/${event.id}`
-         let alertStyle = event.alert ? {color: 'red'} : null
-        let headingStyle = (event.title.length > 6 && this.state.expand)? 'Day-Events_heading-small' : 'Day-Events_heading-large'
+        let alertStyle = event.alert ? { color: 'red' } : null
+        let headingStyle = (event.title.length > 6 && this.state.expand) ? 'Day-Events_heading-small' : 'Day-Events_heading-large'
         let time = convertTime(event.time)
         let timeNumbers = time.split('').slice(0, time.length - 2).join('')
         let timeLetters = time.split('').slice(time.length - 2).join('')
-        
+
         const notify = () => toast((t) => (
-            <span className = 'alert-message'>
-               <b>{event.Event_Host===user?`You`: event.Event_Host}</b>
-        {'  '}changed this event.
-        <br/>
-              <button onClick={() => toast.dismiss(t.id)}>
-                Cool
+            <span className='alert-message'>
+                <b>{event.Event_Host === user ? `You` : event.Event_Host}</b>
+                {'  '}changed this event.
+                <br />
+                <button onClick={() => toast.dismiss(t.id)}>
+                    Cool
               </button>
             </span>
-       
-          ) 
-              ,WekendApiService.turnOffAlert(event.id)
-         
-            .then(this.context.changeEventAlert(event.id,user))
-              .catch(error => { console.error({ error }) })
-            
-          );
-         const host = event.Event_Host ===user ? <>
-           <span className='host-name'>You're</span> hosting
-         </> 
-         : <div>
-         <MediaQuery minDeviceWidth={700}>
-         {(matches) =>
-        matches
-          ?   <div><span className='host-name'>{event.Event_Host}'s</span> hosting</div>
-          :   <div><span className='host-name'>{hostName}'s</span> hosting</div>
-         
-      }
-         </MediaQuery>
-         </div>
-       
+
+        )
+            , WekendApiService.turnOffAlert(event.id)
+
+                .then(this.context.changeEventAlert(event.id, user))
+                .catch(error => { console.error({ error }) })
+
+        );
+        const host = event.Event_Host === user
+            ? <>
+                <span className='host-name'>You're</span> hosting
+            </>
+            : <MediaQuery minDeviceWidth={700}>
+                {(matches) =>
+                    matches
+                        ? <div><span className='host-name'>{event.Event_Host}'s</span> hosting</div>
+                        : <div><span className='host-name'>{hostName}'s</span> hosting</div>
+                }
+            </MediaQuery>
+
+
         const eventDetails = this.state.expand
 
-            ?<div className = 'Event-top-and-bottom'>
-             <div className='Event-content-details'>
-                <div className="Event-column-1">
-                  {host} <br/>
+            ? <div className='Event-top-and-bottom'>
+                <div className='Event-content-details'>
+                    <div className="Event-column-1">
+                        {host} <br />
                   @ {event.place}
+                    </div>
+                    <div className="Event-column-2">
+                        {timeNumbers}
+                        <div className='time-letters'>{timeLetters}</div>
+                    </div>
                 </div>
-                <div className="Event-column-2">
-                    {timeNumbers}
-                    <div className='time-letters'>{timeLetters}</div>
-                </div>
-            </div>
-            <div className = 'event-details'>
-    <div className='Event-bottom-row'> 
-   <span className = 'deets'> deets:</span>{event.details.length===0? 'No deets':event.details.split(/\n \r|\n/).map((para, i) =>
-                    <p key={i}>{para}</p>)}
-          </div>          
-    {event.Event_Host === user 
-     ?<div className='Event-edit-container'>
-                                 <Link
-                        to={`/change-event/${event.id}`}
-                        className='Event-edit-link'>
-                        edit
+                <div className='event-details'>
+                    <div className='Event-bottom-row'>
+                        <span className='deets'> deets:</span>{event.details.length === 0 ? 'No deets' : event.details.split(/\n \r|\n/).map((para, i) =>
+                            <p key={i}>{para}</p>)}
+                    </div>
+                    {event.Event_Host === user
+                        ? <div className='Event-edit-container'>
+                            <Link
+                                to={`/change-event/${event.id}`}
+                                className='Event-edit-link'>
+                                edit
                      </Link>
-                     <Link
-                        to={{pathname:`/delete-event/${event.id}`,
-                            state:{ title: event.title  }
-                        }}
-                        className='Event-edit-link'>
-                        delete
+                            <Link
+                                to={{
+                                    pathname: `/delete-event/${event.id}`,
+                                    state: { title: event.title }
+                                }}
+                                className='Event-edit-link'>
+                                delete
                      </Link>
                         </div>
-                     : <div className='Event-edit-container'>
-                     <Link
-            to={{pathname: path,
-            state: {
-                title: event.title
-            }}}
-            className='Event-edit-link'>
-            {attending?`change rsvp`: `rsvp`}
-         </Link>
-            </div>}
+                        : <div className='Event-edit-container'>
+                            <Link
+                                to={{
+                                    pathname: path,
+                                    state: {
+                                        title: event.title
+                                    }
+                                }}
+                                className='Event-edit-link'>
+                                {attending ? `change rsvp` : `rsvp`}
+                            </Link>
+                        </div>}
                 </div>
-               
-                </div>
+
+            </div>
             : null
-            
-       const alertOrExpand = event.alert? notify : this.handleExpand
+
+        const alertOrExpand = event.alert ? notify : this.handleExpand
 
         return (
-            <div>
+            <>
                 <div className='Day-Events'>
-          
-     
-      <Toaster position="top-center"/>
-
+                    <Toaster position="top-center" />
                     <button
                         type='button'
                         aria-label='expand'
@@ -138,25 +137,25 @@ export default class DayEvent extends Component {
                         <div className="Event-Content">
                             <div className='event-title-attendees'>
                                 <h2 className={headingStyle} style={alertStyle}>{event.title}</h2>
-                                {this.state.expand && 
-                                <>
-                                <span className='whos-going'>Who's going:</span> 
-                                <ul className = 'events-attendees'>
-                                    
-                                   { event.attendees.length === 0? <p>Nobody</p>:event.attendees.map((el,i)=>{
-                                   return <li key={i+event.id} className={el.alert?'alert-on':'alert-off'}>{el.username}</li>
-                                })}
-                                </ul> 
-                                </>}
+                                {this.state.expand &&
+                                    <>
+                                        <span className='whos-going'>Who's going:</span>
+                                        <ul className='events-attendees'>
+
+                                            {event.attendees.length === 0 ? <p>Nobody</p> : event.attendees.map((el, i) => {
+                                                return <li key={i + event.id} className={el.alert ? 'alert-on' : 'alert-off'}>{el.username}</li>
+                                            })}
+                                        </ul>
+                                    </>}
                             </div>
-                   
+
                             {eventDetails}
 
                         </div>
                     </button>
                 </div>
-    
-            </div>
+
+            </>
         )
     }
 }
